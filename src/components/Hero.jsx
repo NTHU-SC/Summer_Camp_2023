0cbc6a49df56a0520382adb9f645e319f64b7749
Nini <ninikingchen@gmail.com>
2023-06-28 22:45:30 +0800

    Added photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 
 import { InvitationModal } from "./InvitationModal";
-import dashboard from "../assets/images/dashboard.jpg";
+import dashboard from "../assets/images/TsingHua.Team.02.jpg";
 
 export const Hero = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
