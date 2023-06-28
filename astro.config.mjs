f6024e91eb9900008d761f74464605159ed88651
William-Mou <william.mou1024@gmail.com>
2023-06-28 22:42:40 +0800

    Setting for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   site: 'https://scc.nthu.site',
-  base: '/summer_camp_2023',
+  base: '/Summer_Camp_2023',
 });
