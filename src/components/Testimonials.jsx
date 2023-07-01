97d11fd16cb97d177facc128736e0bad74e6a593
Yi Kuo <kuokuoyiyi@gmail.com>
2023-07-01 22:32:36 +0800

    feat(Testimonials): seperate row data

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ import testimonial1 from "../assets/images/testimonial1.png";
 import testimonial2 from "../assets/images/testimonial2.png";
 import testimonial3 from "../assets/images/testimonial3.png";
 
-const testimonialsData = [
+const testimonialsDataUpperRow = [
   {
     customerName: "黃恩明",
     customerTitle: "資工系特選學生、超算比賽隊員",
@@ -26,8 +26,31 @@ const testimonialsData = [
     content:
       "魔鬼往往藏在細節裡，看似簡單的系統管理，往往會決定整個系統的效率",
     image: testimonial3,
+  }
+];
+
+const testimonialsDataLowerRow = [
+  {
+    customerName: "黃恩明",
+    customerTitle: "資工系特選學生、超算比賽隊員",
+    content:
+      "超算不僅考驗自己的學理知識，也需要有相當的實作技能，才能勝任比賽任務",
+    image: testimonial1,
   },
-  
+  {
+    customerName: "牟展佑",
+    customerTitle: "大家的隊長",
+    content:
+      "唯有從上而下的理解整個系統的運作，才能榨出機器的完整性能",
+    image: testimonial2,
+  },
+  {
+    customerName: "吳邦寧",
+    customerTitle: "SCC 世界冠軍隊員",
+    content:
+      "魔鬼往往藏在細節裡，看似簡單的系統管理，往往會決定整個系統的效率",
+    image: testimonial3,
+  }
 ];
 
 export const Testimonials = () => (
@@ -48,7 +71,7 @@ export const Testimonials = () => (
         </div>
 
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 py-10 xl:px-0 items-center">
-          {testimonialsData.map((testimonial, index) => (
+          {testimonialsDataUpperRow.map((testimonial, index) => (
             <div
               className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4"
               key={`${testimonial.customerName}-${index}`}
@@ -75,7 +98,7 @@ export const Testimonials = () => (
         </div>
 
         <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 xl:gap-10 px-6 xl:px-0 items-center">
-          {testimonialsData.map((testimonial, index) => (
+          {testimonialsDataLowerRow.map((testimonial, index) => (
             <div
               className="w-11/12 sm:w-4/5 md:w-[560px] lg:w-1/3 custom-border-gray-darker rounded-xl bg-customDarkBg3 flex flex-col px-6 py-4"
               key={`${testimonial.customerName}-${index}`}
