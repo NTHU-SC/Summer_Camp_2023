a72495e6f17fba56ecf6ebf597eee69b0d4d2100
Nini <ninikingchen@gmail.com>
2023-07-01 22:13:56 +0800

    Changed brands and Coc wording and added mail

--- src/components/CoC.jsx
+++ src/components/CoC.jsx
@@ -21,13 +21,13 @@ export const CoC = () => (
               行為準則
             </h2>
             <p className="mb-12 text-customGrayText leading-loose">
-              HPC x AI Summer Camp 歡迎不同身分、來自不同背景的與會者，也非常鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快的參加 HPC x AI，我們要求所有參與者閱讀以下行為準則（Code of Conduct），共同創造一個友善的環境。
+              HPC x AI Summer Camp 歡迎不同身分、來自不同背景的參與者，也非常鼓勵女性、性少數與多元背景的參與者。為了讓大家都能愉快的參加 HPC x AI，我們要求所有參與者閱讀以下行為準則（Code of Conduct），共同創造一個友善的環境。
               
               
               <br />
               <br />
 
-              為此，無論是觀眾、贊助商、工作人員還是講者，希望每位參與者能夠遵守以下行為準則：
+              為此，無論是參與者、贊助商、工作人員還是講師，希望每位參與者能夠遵守以下行為準則：
               <br />
               <br />
               <ul className="mb-6 text-white">
@@ -38,22 +38,22 @@ export const CoC = () => (
                   <span>✔︎ 避免冒犯：避免使用帶有侮辱、歧視或可能引起騷擾的言語和手勢。</span>
                 </li>
                 <li className="mb-4 flex">
-                  <span>✔︎ 關心他人：多關心周圍的社群夥伴，詢問他們是否需要幫助。</span>
+                  <span>✔︎ 關心他人：多關心周圍的夥伴，詢問他們是否需要幫助。</span>
                 </li>
                 <li className="mb-4 flex">
                   <span>✔︎ 尋求協助：當遇到危險或發現事情不對勁時，及時尋求工作人員的支援和協助。</span>
                 </li>
                 <li className="mb-4 flex">
-                  <span>✔︎ 交朋友：在年會中多結交朋友，珍惜彼此相聚的時光。</span>
+                  <span>✔︎ 交朋友：在營隊中多結交朋友，珍惜彼此相聚的時光。</span>
                 </li>
               </ul>
               <br />
-              以下行為是社群不容忍的：
+              以下行為是營隊不容忍的：
               <br />
               <br />
               <ul className="mb-6 text-white">
                 <li className="mb-4 flex">
-                  <span>⛌  挑釁和冒犯：針對性別、性取向、種族、外貌、宗教、年齡、身體狀況或個人身份的挑釁或冒犯。</span>
+                  <span>⛌ 挑釁和冒犯：針對性別、性取向、種族、外貌、宗教、年齡、身體狀況或個人身份的挑釁或冒犯。</span>
                 </li>
                 <li className="mb-4 flex">
                   <span>⛌ 騷擾：任何形式的性騷擾、言語和肢體霸凌。</span>
@@ -62,7 +62,7 @@ export const CoC = () => (
                   <span>⛌ 不當言論和展示：公開展示或放映含有侮辱、歧視、仇恨、暴力或性暗示的言論、圖像或錄影。</span>
                 </li>
                 <li className="mb-4 flex">
-                  <span>⛌ 干擾活動：無故干擾議程或活動的正常進行，無視工作人員或其他參與者的制止。</span>
+                  <span>⛌ 干擾活動：無故干擾營隊活動的正常進行，無視工作人員或其他參與者的制止。</span>
                 </li>
                 <li className="mb-4 flex">
                   <span>⛌ 違法行為：從事違反法律的行為。</span>
